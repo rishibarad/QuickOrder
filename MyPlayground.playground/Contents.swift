d58235eb655cb9c82a7b0ff30452cf7c1d9c3961
Rishi Barad <rishib@umich.edu>
2019-12-20 15:01:15 -0500

    started playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+//Playground
+//Hacking with Swift Tutorials. Credit to John Hudson
+
+import UIKit
+
+//method 1: less preferred
+var name: String
+name = "Tim McGraw"
+var age: Int
+age = 25
+
+var latitude: Double
+latitude = -86.784834483
+
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+//better method
+
+var name2 = "Tim McGee"
+var age2 = 26
+var latitude2 = -86.435345
+var stayOutTooLate2 = true;
+
